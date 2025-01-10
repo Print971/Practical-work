58be78292166b68e08088a237ad7c2725f590677
Print971 <Yapanski@yandex.ru>
2025-01-10 16:52:17 +0400

    Add files via upload

---
+++ one.py
@@ -0,0 +1,11 @@
+print(12 + 89 - 1)
+print(9**0.5*5)#1st program
+print(9.99>9.98 and 1000!=1000.1)#2nd program
+print(2 * 2 + 2 )#3rd program
+print(2 * ( 2 + 2 ))
+print(6==8)
+print(type(float("123.456")))#4rt program
+print(123.456 * 10)
+print(1234.56)
+print(int(1234.56))
+print(int(1234 % 10))
